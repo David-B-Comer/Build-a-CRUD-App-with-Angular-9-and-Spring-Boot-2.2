ea4e4f1470538787a167721f7e5e5804f69f52fc
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-06-04 15:06:43 -0400

    added notes

--- notes/src/app/app.component.spec.ts
+++ notes/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ls'`, () => {
+  it(`should have as title 'notes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ls');
+    expect(app.title).toEqual('notes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ls app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('notes app is running!');
   });
 });
