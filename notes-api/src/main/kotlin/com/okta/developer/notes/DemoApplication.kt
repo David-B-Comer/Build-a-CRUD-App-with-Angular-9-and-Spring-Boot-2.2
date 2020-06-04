dc2f013bcf08f29a1621acd1b4e1464dcbb19dd7
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-06-04 16:35:00 -0400

    added entity to demo applicagion of notes api

--- notes-api/src/main/kotlin/com/okta/developer/notes/DemoApplication.kt
+++ notes-api/src/main/kotlin/com/okta/developer/notes/DemoApplication.kt
@@ -1,11 +1,21 @@
 package com.okta.developer.notes
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
+
+@Entity
+data class Note(@Id @GeneratedValue var id: Long? = null,
+                var title: String? = null,
+                var text: String? = null,
+                @JsonIgnore var user: String? = null)
